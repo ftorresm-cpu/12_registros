87270ec34a05d362a9e448adcad9db6557255f06
ftorresm-cpu <ftorresm@unjbg.edu.pe>
2026-06-24 09:23:09 -0500

    Estructura de libros.cpp

--- libros.cpp
+++ libros.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+struct Libro{
+    string titulo;
+    string autor;
+    string editorial;
+    int anioPublicacion;
+};
+
 int main(){
-    return
+    
+    return 0;
 }
