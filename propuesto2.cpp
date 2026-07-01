d7ae38fb84c92847e248cf1362de0f0ef113e45a
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 20:39:50 -0500

    Propuesto2: Estructura de registro Persona creada

---
+++ propuesto2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Persona {
+    string nombre;
+    string dni;
+    int edad;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    
+    Persona lista[100];
+    
+    return 0;
+}
