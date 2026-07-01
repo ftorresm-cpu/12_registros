e5127ac85d7cbd6e9a2163b0a229e58fa41d8ea9
ftorresm <ftorresm@unjbg.edu.pe>
2026-07-01 14:58:11 -0500

    Empleados: Estructura del registro con arreglo interno de ventas

---
+++ empleados.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Empleado {
+    int numero;
+    string nombre;
+    float ventas[12];
+    float salario;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    
+    Empleado lista[100];
+    
+    return 0;
+}
