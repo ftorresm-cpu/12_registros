88be976779f4068cdd6d7a55f9d01130198a6b67
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 20:31:30 -0500

    Propuesto1: Registro y lectura de datos con ciclo for

--- propuesto1.cpp
+++ propuesto1.cpp
@@ -14,5 +14,11 @@ int main() {
     
     Empleado lista[100];
     
+    for (int i = 0; i < n; i++) {
+        cin >> lista[i].nombre;
+        cin >> lista[i].sexo;
+        cin >> lista[i].sueldo;
+    }
+    
     return 0;
 }
