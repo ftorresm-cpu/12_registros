bd83a838880e6349e56a876521145e55533c729a
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 21:20:16 -0500

    Propuesto3: Agregado el ciclo for para lectura de datos

--- propuesto3.cpp
+++ propuesto3.cpp
@@ -15,5 +15,12 @@ int main() {
     
     Persona lista[100];
     
+    for (int i = 0; i < n; i++) {
+        cin >> lista[i].nombre;
+        cin >> lista[i].dia;
+        cin >> lista[i].mes;
+        cin >> lista[i].anio;
+    }
+    
     return 0;
 }
