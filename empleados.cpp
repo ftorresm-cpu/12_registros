5451402da1290e6400f01a7e1743789939e63d7d
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-01 09:28:30 -0500

    Empleados: estructura del registro con arreglo en ventas

---
+++ empleados.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Empleado {
+	int numero;
+	string nombre;
+	float ventas[12];
+	float salario;
+};
+int main(){
+	int n;
+	cin>>n;
+	
+	Empleado lista[100];
+	
+	return 0;
+}
