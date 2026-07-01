a293c2127a0fb877ff57b74efd43a7b4901d0671
ftorresm-cpu <ftorresm@unjbg.edu.pe>
2026-07-01 09:43:18 -0500

    Empleados: Lectura de registros y arreglos

--- empleados.cpp
+++ empleados.cpp
@@ -13,6 +13,14 @@ int main(){
 	cin>>n;
 	
 	Empleado lista[100];
+	for (int i =; i < n; i++){
+		cin>> lista[i].numero;
+		cin>> lista[i].nombre;
+		cin>> lista[i].salario;
+		
+		for (int m = 0; m < 12; m++)
+		cin>> lista[i].ventas[m];
+	}
 	
 	return 0;
 }
