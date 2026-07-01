eaded7faebf6ec803dde65dc58305fd34eb1c2f2
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 20:58:21 -0500

    Propuesto2: Ejercicio completo con calculos de edad

--- propuesto2.cpp
+++ propuesto2.cpp
@@ -1,4 +1,4 @@
-##include <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
@@ -14,10 +14,27 @@ int main() {
     
     Persona lista[100];
     
+    int sumaEdades = 0;
+    int mayoresDe50 = 0;
+    
     for (int i = 0; i < n; i++) {
         cin >> lista[i].nombre;
         cin >> lista[i].dni;
         cin >> lista[i].edad;
+        
+        sumaEdades = sumaEdades + lista[i].edad;
+        
+        if (lista[i].edad > 50) {
+            mayoresDe50 = mayorsDe50 + 1;
+        }
+    }
+    
+    float promedio = (float)sumaEdades / n;
+    cout << mayoresDe50 << endl;
+    cout << promedio << endl;
+    
+    for (int i = 0; i < n; i++) {
+        cout << lista[i].nombre << " " << lista[i].dni << " " << lista[i].edad << endl;
     }
     
     return 0;
