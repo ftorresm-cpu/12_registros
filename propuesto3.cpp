38be08ca17fa5f83268b69be7e6a7f138443b1cf
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 21:02:27 -0500

    Propuesto3: Registro Persona y variables iniciales creadas

---
+++ propuesto3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Persona {
+    string nombre;
+    int dia;
+    int mes;
+    int anio;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    
+    Persona lista[100];
+    
+    return 0;
+}
