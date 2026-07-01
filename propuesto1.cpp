872b53608f1a14f5b776bd53a667281b1b6560c6
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 20:27:56 -0500

    Propuesto1: Estructura base del registro de empleados

---
+++ propuesto1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Empleado {
+    string nombre;
+    char sexo;
+    float sueldo;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    
+    Empleado lista[100];
+    
+    return 0;
+}
