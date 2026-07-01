0e16b2bcaada2d4782ac7635bf292f945efc1acc
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 21:27:34 -0500

    Propuesto4: Registro Atleta y variables base creadas

---
+++ propuesto4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Atleta {
+    string nombre;
+    string pais;
+    string disciplina;
+    int medallas;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    
+    Atleta lista[100];
+    
+    return 0;
+}
