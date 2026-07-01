f6e6760bbf1020dd5ff243150e6434543514c713
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 21:24:29 -0500

    Propuesto3: Ejercicio completo usando ciclo while de busqueda

--- propuesto3.cpp
+++ propuesto3.cpp
@@ -22,5 +22,18 @@ int main() {
         cin >> lista[i].anio;
     }
     
+    int mesBuscado;
+    cin >> mesBuscado;
+    
+    while (mesBuscado != 0) {
+        
+        for (int i = 0; i < n; i++) {
+            if (lista[i].mes == mesBuscado) {
+                cout << lista[i].nombre << " " << lista[i].dia << " " << lista[i].mes << " " << lista[i].anio << endl;
+            }
+        }
+        cin >> mesBuscado;
+    }
+    
     return 0;
 }
