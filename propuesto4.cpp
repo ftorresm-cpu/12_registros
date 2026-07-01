48725b13a13eac47ab04791b58de6dde5fb2f3cc
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-30 22:11:57 -0500

    Propuesto4: Ejercicio completo con filtro por pais y medallas

--- propuesto4.cpp
+++ propuesto4.cpp
@@ -22,5 +22,25 @@ int main() {
         cin >> lista[i].medallas;
     }
     
+    string paisBuscado;
+    cin >> paisBuscado;
+    
+    Atleta mejorAtleta;
+    mejorAtleta.medallas = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (lista[i].pais == paisBuscado) {
+            cout << lista[i].nombre << " " << lista[i].pais << " " << lista[i].disciplina << " " << lista[i].medallas << endl;
+            
+            if (lista[i].medallas > mejorAtleta.medallas) {
+                mejorAtleta = lista[i];
+            }
+        }
+    }
+    
+    if (mejorAtleta.medallas != -1) {
+        cout << mejorAtleta.nombre << " " << mejorAtleta.medallas << endl;
+    }
+    
     return 0;
 }
