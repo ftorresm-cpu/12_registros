84d52ff0c62618135262d10a65f0ba04211072bf
ftorresm <ftorresm@unjbg.edu.pe>
2026-07-01 21:09:09 -0500

    Contactos: Version final con menu vertical limpio

--- contactos.cpp
+++ contactos.cpp
@@ -15,8 +15,18 @@ int main() {
     int opcion = -1;
     
     while (opcion != 0) {
-        cout << "1. Agregar  2. Modificar  3. Listar  4. Servidor  5. Eliminar  6. Buscar  0. Salir" << endl;
+        cout << "\n--- MENU DE CONTACTOS ---" << endl;
+        cout << "1. Agregar contacto" << endl;
+        cout << "2. Modificar contacto" << endl;
+        cout << "3. Listar contactos" << endl;
+        cout << "4. Buscar por servidor" << endl;
+        cout << "5. Eliminar contacto" << endl;
+        cout << "6. Buscar por email" << endl;
+        cout << "0. Salir del programa" << endl;
+        cout << "-------------------------" << endl;
+        cout << "Seleccione una opcion: ";
         cin >> opcion;
+        cout << endl;
         
         if (opcion == 1) {
             cin >> agenda[total].nombre;
@@ -25,20 +35,57 @@ int main() {
             cin >> agenda[total].email;
             total = total + 1;
             
+        } else if (opcion == 2) {
+            string correo;
+            cin >> correo;
+            for (int i = 0; i < total; i++) {
+                if (agenda[i].email == correo && agenda[i].nombre != "VACIO") {
+                    cin >> agenda[i].nombre;
+                    cin >> agenda[i].sexo;
+                    cin >> agenda[i].edad;
+                    cin >> agenda[i].email;
+                }
+            }
+            
         } else if (opcion == 3) {
             for (int i = 0; i < total; i++) {
-                cout << agenda[i].nombre << " " << agenda[i].sexo << " " << agenda[i].edad << " " << agenda[i].email << endl;
+                if (agenda[i].nombre != "VACIO") {
+                    cout << agenda[i].nombre << " " << agenda[i].sexo << " " << agenda[i].edad << " " << agenda[i].email << endl;
+                }
+            }
+            
+        } else if (opcion == 4) {
+            string servidor;
+            cin >> servidor;
+            for (int i = 0; i < total; i++) {
+                if (agenda[i].nombre != "VACIO") {
+                    if (agenda[i].email.find(servidor) != string::npos) {
+                        cout << agenda[i].nombre << " " << agenda[i].email << endl;
+                    }
+                }
+            }
+            
+        } else if (opcion == 5) {
+            string correo;
+            cin >> correo;
+            for (int i = 0; i < total; i++) {
+                if (agenda[i].email == correo) {
+                    agenda[i].nombre = "VACIO";
+                    cout << "Contacto borrado" << endl;
+                }
             }
             
         } else if (opcion == 6) {
             string buscado;
             cin >> buscado;
             for (int i = 0; i < total; i++) {
-                if (agenda[i].email == buscado) {
+                if (agenda[i].email == buscado && agenda[i].nombre != "VACIO") {
                     cout << "Posicion: " << i << endl;
                     cout << agenda[i].nombre << " " << agenda[i].email << endl;
                 }
             }
+        } else if (opcion < 0 || opcion > 6) {
+            cout << "Opcion invalida. Intente de nuevo." << endl;
         }
     }
     
